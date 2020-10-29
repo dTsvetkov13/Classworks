dec104c7f22f101eacf0ed08a82f413752002438
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2020-10-29 18:20:03 +0200

    Event init

---
+++ Event/models/Event.java
@@ -0,0 +1,62 @@
+package oop.classes.event.models;
+
+import oop.classes.Date.models.Date;
+
+public class Event
+{
+	private Date date;
+	private String name;
+	
+	public Event()
+	{
+		setDate(new Date());
+		setName("");
+	}
+	
+	public Event(Date date, String name)
+	{
+		setDate(date);
+		setName(name);
+	}
+	
+	public Date getDate()
+	{
+		return this.date;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setDate(Date date)
+	{
+		if(date == null)
+		{
+			throw new RuntimeException("Date is null in Event's constructor");
+		}
+		
+		this.date = date;
+	}
+	
+	public void setName(String name)
+	{
+		if(name == null || name.isEmpty())
+		{
+			throw new RuntimeException("Invalid name in Event's constructor");
+		}
+		
+		this.name = name;
+	}
+	
+	public String toString()
+	{
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		stringBuilder.append(name);
+		stringBuilder.append(" at ");
+		stringBuilder.append(date.ToString());
+		
+		return stringBuilder.toString();
+	}
+}
